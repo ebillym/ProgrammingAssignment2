8a11c072d83e4f2764a15656a4250ba9de15fcf0
Billy Meng <billy.meng@ericsson.com>
2014-10-19 20:45:17 +0200

    Added requirement as comment in the beginning of the code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+## Requirement
+## Write the following functions:
+##    
+##    makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+##    cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+
 ## The first function, makeCacheMatrix creates a special "matrix", 
 ## which is really a matrix containing functions to
 ## 1. set the value of the matrix
